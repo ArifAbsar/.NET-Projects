5d2913771989c72eedd91b8cb25c755271209990
Arif Absar <arifabsar182@gmail.com>
2024-02-17 22:31:50 +0600

    database connection

---
+++ Login/DataObjects/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Login.DataObjects
+{
+    public partial class User
+    {
+        public string UserId { get; set; }
+        public string Name { get; set; }
+        public string UName { get; set; }
+        public string Pass { get; set; }
+    }
+}
---
+++ Login/DataObjects/crudContext.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Login.DataObjects
+{
+    public partial class CrudContext : DbContext
+    {
+        public CrudContext()
+        {
+        }
+
+        public CrudContext(DbContextOptions<CrudContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<User> User { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                string connectionString = ConfigurationManager.ConnectionStrings["MyConnectionString"].ConnectionString;
+
+                // Use the retrieved connection string with optionsBuilder.UseMySQL
+                optionsBuilder.UseMySQL(connectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.ToTable("user");
+
+                entity.Property(e => e.UserId)
+                    .HasColumnName("USER_ID")
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Pass)
+                    .IsRequired()
+                    .HasColumnName("PASS")
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.UName)
+                    .IsRequired()
+                    .HasColumnName("U_NAME")
+                    .HasMaxLength(50);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
