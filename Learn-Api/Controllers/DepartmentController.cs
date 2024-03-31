c124f2fc3cbdcc735e067f91adc0902551efb841
Arif Absar <arifabsar182@gmail.com>
2024-04-01 02:54:25 +0600

    started learning api

---
+++ Learn-Api/Controllers/DepartmentController.cs
@@ -0,0 +1,35 @@
+﻿using Learn_Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Learn_Api.Controllers
+{
+    public class DepartmentController : ApiController
+    {
+        [HttpGet]
+        [Route("api/department/get")]
+        public HttpResponseMessage Getname()
+        {
+            string[] name = { "Theory Of Computation", "Compiler Design", "Data Structure" };
+            return Request.CreateResponse(HttpStatusCode.OK,name);
+        }
+        [HttpGet]
+        [Route("api/department/name/{p_name}")]
+        public HttpResponseMessage Getdept(string[] p_name)
+        {
+            
+           
+            return Request.CreateResponse(HttpStatusCode.OK,p_name);
+        }
+        [HttpPost]
+        [Route("api/department/create")]
+        public HttpResponseMessage Add(Department p)
+        {
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+    }
+}
---
+++ Learn-Api/Models/Department.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Learn_Api.Models
+{
+    public class Department
+    {
+        public  string name { get; set; }
+        public int id { get; set; }
+        public int credit { get; set; }
+    }
+}
